4b8e7c8d3c4028cc7e5ccd9a0f7e75281e627b3c
ertuy <valerius1999@gmail.com>
2020-04-24 12:48:52 +0300

    Add files via upload

--- form.py
+++ form.py
@@ -1,18 +1,9 @@
-
+from django import forms
+from django.http import Http404
 from .models import Article
 from django.shortcuts import render,redirect
-from django.http import Http404
-def get_article(request, article_id):
-    try:
-        post = Article.objects.get(id=article_id)
-        return render(request,'article.html',{"post": post})
-    except Article.DoesNotExist:
-        raise Http404
-
-def archive(request):
-    return render(request, 'archive.html', {"posts": Article.objects.all()})
 def create_post(request):
-    if not request.user.is_anonymous:
+    if not request.user.is_anonymous():
         if request.method == "POST":
             form = {
                 'text': request.POST["text"],
@@ -22,10 +13,10 @@ def create_post(request):
                 Article.objects.create(text=form["text"],
                                        title=form["title"],
                                        author=request.user)
-                return redirect('get_article', article_id=article_id)
+                return redirect('get_article', article_id=article.id)
             else:
                 form['errors'] = u"Не все поля заполнены"
-                return render(request, 'post_edit.html', {'form': form})
+                return render(request, 'create_post.html', {'form': form})
         else:
             return render(request, 'post_edit.html', {})
 
